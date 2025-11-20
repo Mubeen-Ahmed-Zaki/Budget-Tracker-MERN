6d66606c020f1bcc1abfd0e70a6b37fc08fec731
Mubeen Ahmed Zaki <140517256+Mubeen-Ahmed-Zaki@users.noreply.github.com>
2025-11-20 16:56:08 +0500

    Update api.jsx

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://budget-tracker-mern-phi.vercel.app",
+  baseURL: "https://budget-tracker-mern-phi.vercel.app/api",
   withCredentials: true,
 });
 
